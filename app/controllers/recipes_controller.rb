ac34166f0aad7b89eb5175fb364fd0db1b35c1f9
Chaim Chazan <chaimyisroel@gmail.com>
2021-10-19 18:02:56 -0400

    middle1

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,6 @@
 class RecipesController < ApplicationController
    before_action :authorize
 
-   # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-   # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
    def index 
       recipes = Recipe.all
       render json: recipes, status: :created
@@ -12,13 +9,12 @@ class RecipesController < ApplicationController
    def create
       user = User.find_by(id: session[:user_id])
       recipe = user.recipes.create(recipe_params)
-      # render json: recipe, status: :created
 
       if recipe.valid?
          recipe.save
          render json: recipe, status: :created
       else
-         render json:{ error: "recipe.errors" }, status: :unprocessable_entity
+         render json: {errors: recipe.errors.full_messages}, status: :unprocessable_entity
       end
 
    end
@@ -33,11 +29,4 @@ class RecipesController < ApplicationController
      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
    end
 
-   #   def render_not_found_response
-   #    render json: { error: "Record Not found" }, status: :not_found
-   # end
-
-   # def render_unprocessable_entity_response(invalid)
-   #    render json: { errors: invalid.errors }, status: :unprocessable_entity
-   #  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,5 @@
 class SessionsController < ApplicationController
 
-   
-   # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-   # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
-
    def create
       user = User.find_by_username(params[:username])
       if user&.authenticate(params[:password])
@@ -25,13 +20,5 @@ class SessionsController < ApplicationController
       end
    end
 
-   private
-   # def render_not_found_response
-   #    render json: { error: "Record Not found" }, status: :not_found
-   # end
-
-   # def render_unauthorized_entity_response(invalid)
-   #    render json: { errors: invalid.record.errors.full_messages }, status: :unauthorized
-   # end
 
 end
